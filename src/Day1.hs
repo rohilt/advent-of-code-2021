24a4e43491d6c54f6dc574fce4ca1d488384a069
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-09 11:40:47 -0500

    refactored day 1 again

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,6 +1,5 @@
 import System.IO
 
-type PreviousThreeDepths = (Int, Int, Int)
 main :: IO()
 main = do
   input <- readFile "in/day1"
@@ -12,14 +11,12 @@ parseInput input = map (\x -> read x :: Int) $ lines input
 
 part1 :: [Int] -> Int
 part1 = (\(_, c) -> c) . foldl countIncreasingDepths (0, -1)
-
-countIncreasingDepths :: (Int, Int) -> Int -> (Int, Int)
-countIncreasingDepths (previousDepth, count) currentDepth =
-  (currentDepth, if previousDepth < currentDepth then count + 1 else count)
+  where
+    countIncreasingDepths (previousDepth, count) currentDepth =
+      (currentDepth, if previousDepth < currentDepth then count + 1 else count)
 
 part2 :: [Int] -> Int
 part2 = (\(_, c) -> c) . foldl countIncreasingDepthsBy3 ((0, 0, 0), -3)
-
-countIncreasingDepthsBy3 :: (PreviousThreeDepths, Int) -> Int -> (PreviousThreeDepths, Int)
-countIncreasingDepthsBy3 ((p1, p2, p3), count) c =
-  ((p2, p3, c), if p1 < c then count + 1 else count)
+  where
+    countIncreasingDepthsBy3 ((p1, p2, p3), count) c =
+      ((p2, p3, c), if p1 < c then count + 1 else count)
