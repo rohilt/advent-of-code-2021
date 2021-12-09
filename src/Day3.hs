7d73a3083ba913ae3bee6e65c65e65d9aa23b25d
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-09 11:03:19 -0500

    set up automation and project structure

---
+++ src/Day3.hs
@@ -0,0 +1,16 @@
+import System.IO
+
+main :: IO()
+main = do
+  input <- readFile "in/day3"
+  print $ part1 $ parseInput input
+  print $ part2 $ parseInput input
+
+parseInput :: String -> Int
+parseInput _ = 0
+
+part1 :: Int -> Int
+part1 _ = 0
+
+part2 :: Int -> Int
+part2 _ = 0
---
+++ src/Day5.hs
@@ -0,0 +1,16 @@
+import System.IO
+
+main :: IO()
+main = do
+  input <- readFile "in/day5"
+  print $ part1 $ parseInput input
+  print $ part2 $ parseInput input
+
+parseInput :: String -> Int
+parseInput _ = 0
+
+part1 :: Int -> Int
+part1 _ = 0
+
+part2 :: Int -> Int
+part2 _ = 0
