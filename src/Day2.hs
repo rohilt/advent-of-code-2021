782d556d97f1c7c58664d2df8275e81d4a339504
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-09 11:52:37 -0500

    completed day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,16 +1,30 @@
 import System.IO
 
+data Instruction = Forward Int | Down Int | Up Int
+
 main :: IO()
 main = do
   input <- readFile "in/day2"
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
 
-parseInput :: String -> Int
-parseInput _ = 0
+parseInput :: String -> [Instruction]
+parseInput = map (formatInstruction . words) . lines
+  where
+    formatInstruction ("forward":x:[]) = Forward (read x :: Int)
+    formatInstruction ("down":x:[]) = Down (read x :: Int)
+    formatInstruction ("up":x:[]) = Up (read x :: Int)
 
-part1 :: Int -> Int
-part1 _ = 0
+part1 :: [Instruction] -> Int
+part1 = (\(h, d) -> h * d) . foldl updatePosition (0, 0)
+  where
+    updatePosition (h, d) (Forward x) = (h + x, d)
+    updatePosition (h, d) (Up x) = (h, d - x)
+    updatePosition (h, d) (Down x) = (h, d + x)
 
-part2 :: Int -> Int
-part2 _ = 0
+part2 :: [Instruction] -> Int
+part2 = (\(h, d, _) -> h * d) . foldl updatePosition (0, 0, 0)
+  where
+    updatePosition (h, d, a) (Forward x) = (h + x, d + (a * x), a)
+    updatePosition (h, d, a) (Up x) = (h, d, a - x)
+    updatePosition (h, d, a) (Down x) = (h, d, a + x)
