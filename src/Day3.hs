4df13954829475f5425deb56a8b31deb167e68aa
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-10 11:22:12 -0500

    completed day 3, part 1

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,4 +1,6 @@
 import System.IO
+import Data.Array
+import Data.Char
 
 main :: IO()
 main = do
@@ -6,11 +8,26 @@ main = do
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
 
-parseInput :: String -> Int
-parseInput _ = 0
+parseInput :: String -> [[Int]]
+parseInput = map (map digitToInt . reverse) . lines
 
-part1 :: Int -> Int
-part1 _ = 0
+part1 :: [[Int]] -> Int
+part1 = getProduct . countBits
 
-part2 :: Int -> Int
+countBits :: [[Int]] -> ([Int], Int)
+countBits numbers = (foldl (zipWith (+)) [0,0..] numbers, length numbers)
+
+getProduct :: ([Int], Int) -> Int
+getProduct (counts, count) = gamma * epsilon
+  where
+    compareTo = div count 2
+    gamma = toBinary $ map (\b -> if b > compareTo then 1 else 0) counts
+    epsilon = toBinary $ map (\b -> if b < compareTo then 1 else 0) counts
+
+toBinary :: [Int] -> Int
+toBinary = (\(_, x) -> x) . foldl handleBinary (0, 0)
+  where
+    handleBinary (i, num) bit = (i+1, ((2^i)*bit) + num)
+
+part2 :: [[Int]] -> Int
 part2 _ = 0
