df156abd577d86bfeb4083cd3afbdf015b85595c
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-12 16:15:40 -0500

    completed day 7, part 1

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,4 +1,6 @@
 import System.IO
+import Data.List
+import Helper.Parse
 
 main :: IO()
 main = do
@@ -6,11 +8,22 @@ main = do
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
 
-parseInput :: String -> Int
-parseInput _ = 0
+parseInput :: String -> [Int]
+parseInput = splitNumsAtComma
 
-part1 :: Int -> Int
-part1 _ = 0
+part1 :: [Int] -> Int
+part1 list = sum $ map fuelCost list
+  where
+    align = median $ sort list
+    fuelCost position = abs (position - align)
 
-part2 :: Int -> Int
+median :: [Int] -> Int
+median list
+  | mod (length list) 2 == 1 = (list !! middle)
+  | mod (length list) 2 == 0 = div ((+) (list !! middle) (list !! middle2)) 2
+    where
+      middle = div (length list) 2
+      middle2 = (-) (div (length list) 2) 1
+
+part2 :: [Int] -> Int
 part2 _ = 0
