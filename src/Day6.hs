bb0f48f5a3c776deb53ad7bd8b5f91610457a64f
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-12 15:50:54 -0500

    completed day 6

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,4 +1,8 @@
 import System.IO
+import qualified Data.Map as Map
+import Helper.Parse
+
+type Fish = Map.Map Int Int
 
 main :: IO()
 main = do
@@ -6,11 +10,19 @@ main = do
   print $ part1 $ parseInput input
   print $ part2 $ parseInput input
 
-parseInput :: String -> Int
-parseInput _ = 0
+parseInput :: String -> Fish
+parseInput = foldl addToMap Map.empty . splitNumsAtComma
+  where
+    addToMap map value = Map.insert value ((+) (Map.findWithDefault 0 value map) 1) map
 
-part1 :: Int -> Int
-part1 _ = 0
+part1 :: Fish -> Int
+part1 = sum . Map.elems . head . drop 80 . iterate iterateDay
 
-part2 :: Int -> Int
-part2 _ = 0
+part2 :: Fish -> Int
+part2 = sum . Map.elems . head . drop 256 . iterate iterateDay
+
+iterateDay :: Fish -> Fish
+iterateDay map = Map.insert 8 newFish $ Map.mapKeysWith (+) updateKey map
+  where
+    newFish = Map.findWithDefault 0 0 map
+    updateKey k = if k == 0 then 6 else k-1
