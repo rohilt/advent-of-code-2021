5d0b1ab3395599b60752f3627466a2aa94cfe3a0
Rohil Tuli <rohil.tuli@gmail.com>
2021-12-12 15:24:40 -0500

    completed day 5, part 2

--- src/Day5.hs
+++ src/Day5.hs
@@ -18,20 +18,29 @@ parseInput = map parseLineSegment . lines
     parsePoint = (\(x:y:[]) -> (x,y)) . splitNumsAtComma
 
 part1 :: [LineSegment] -> Int
-part1 = length . filter (> 1) . Map.elems . foldl createMap Map.empty . concat . map (points False)
-  where
-    createMap map point = Map.insert point ((+) (Map.findWithDefault 0 point map) 1) map
+part1 = countIntersections . map (points False)
+
+part2 :: [LineSegment] -> Int
+part2 = countIntersections . map (points True)
+
+countIntersections :: [[Point]] -> Int
+countIntersections = length . filter (> 1) . Map.elems . foldl createMap Map.empty . concat
+
+createMap :: Map.Map Point Int -> Point -> Map.Map Point Int
+createMap map point = Map.insert point ((+) (Map.findWithDefault 0 point map) 1) map
 
 points :: Bool -> LineSegment -> [Point]
 points countDiagonals ((x1, y1), (x2, y2))
   | x1 == x2  = map (\y -> (x1, y)) $ range y1 y2
   | y1 == y2  = map (\x -> (x, y1)) $ range x1 x2
-  | otherwise =  []
+  | otherwise = if countDiagonals then zip (range2 x1 x2) (range2 y1 y2) else []
 
 range :: Int -> Int -> [Int]
 range x y
   | x < y     = [x..y]
   | otherwise = [y..x]
 
-part2 :: [LineSegment] -> Int
-part2 _ = 0
+range2 :: Int -> Int -> [Int]
+range2 x y
+  | x < y     = [x..y]
+  | otherwise = reverse $ [y..x]
